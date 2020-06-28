dd575e50cbc6e46bc34ce709691a69b1d080e4c9
Ansuel Smith <ansuelsmth@gmail.com>
2020-06-28 21:44:30 +0200

    Comment and add test program
    - Comment some bitmap function
    - Start test the disk init function
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

--- bitmap.h
+++ bitmap.h
@@ -15,11 +15,14 @@ typedef struct {
 BitMapEntryKey BitMap_blockToIndex(int num);
 
 // converts a bit to a linear index
+// Converte l'indice nella bitmap (entry) nell'offset del fd rappresentate il nostro disco (contenuto in un file statico)
 int BitMap_indexToBlock(int entry, uint8_t bit_num);
 
 // returns the index of the first bit having status "status"
 // in the bitmap bmap, and starts looking from position start
+// Da quanto abbiamo capito questo serve per trovare il primo blocco libero all'interno della bitmap
 int BitMap_get(BitMap* bmap, int start, int status);
 
 // sets the bit at index pos in bmap to status
+// Setta il blocco in posizione pos libero o occupato (status da quanto abbiamo capito viene utilizzato per settare il blocco con dati o sovrascrivibile(senza dati))
 int BitMap_set(BitMap* bmap, int pos, int status);
--- simplefs_test.c
+++ simplefs_test.c
@@ -1,9 +1,22 @@
 #include "simplefs.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#define DISK_NAME "test.fs"
+
 int main(int agc, char** argv) {
   printf("FirstBlock size %ld\n", sizeof(FirstFileBlock));
   printf("DataBlock size %ld\n", sizeof(FileBlock));
   printf("FirstDirectoryBlock size %ld\n", sizeof(FirstDirectoryBlock));
   printf("DirectoryBlock size %ld\n", sizeof(DirectoryBlock));
   
+  DiskDriver *disk = malloc(sizeof(DiskDriver));
+
+  DiskDriver_init(disk, DISK_NAME, 0);
+
+  printf("%d", disk->header->num_blocks);
+
+  disk->header->num_blocks = 43;
+
+
 }
